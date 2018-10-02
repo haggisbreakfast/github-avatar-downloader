acaf0d79f5410c551bc434dd808f06783b74c127
Lindsay Stewart <lj.stewart00@gmail.com>
2018-10-02 04:17:53 +0000

    added conditionals for command line arguments

--- download_avatars.js
+++ download_avatars.js
@@ -5,7 +5,7 @@ var inputOne = process.argv[2];
 var inputTwo = process.argv[3];
 
 
-// file into
+// welcome msg
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 //
@@ -16,7 +16,13 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'User-Agent': 'request',
       'Authorization': 'token ' + secrets.GITHUB_TOKEN
     }
+
   };
+  // add conditionals should arguments not be provided on command line
+  if (inputOne === undefined || inputTwo === undefined) {
+    console.log("There is an error with your arguments!");
+    return false;
+  }
 
   request(options, function (err, res, body) {
     var parsedBody = JSON.parse(body);
@@ -30,7 +36,6 @@ function getRepoContributors(repoOwner, repoName, cb) {
 function downloadImageByURL(url, filePath) {
   request(url).pipe(fs.createWriteStream(filePath));
 }
-
 
 getRepoContributors(inputOne, inputTwo, function (err, result) {
   var url = result.avatar_url;
